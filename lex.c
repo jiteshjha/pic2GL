85be159a0617136d63c9ce727fb8592c0bee05dc
jiteshjha <jiteshjha96@gmail.com>
2017-03-26 00:02:40 +0530

    Command

--- lex.c
+++ lex.c
@@ -1,236 +1,256 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include < stdio.h > 
+#include < stdlib.h > 
+#include < string.h >
 
-/**
-<primitive> ::=
-  box                   # closed object — rectangle
-  circle                # closed object — circle
-  ellipse               # closed object — ellipse
-  arc                   # open object — quarter-circle
-  line                  # open object — line
-  arrow                 # open object — line with arrowhead
-  spline                # open object — spline curve
-  move
-  TEXT TEXT ...         # text within invisible box
-  plot <expr> TEXT      # formatted text
-  ’[’ <command> ... ’]’
-**/
-char *primitive[10] = {"box", "circle", "ellipse", "arc", "line", "arrow", "spline", "move", "plot", "["};
-/**
-    up | down | left | right
-**/
-char *direction[4] = {"up", "down", "left", "right"};
+    char * direction_set[4] = {
+        "up",
+        "down",
+        "left",
+        "right"
+    };
 
-char spec_char[8] = {'(',')','{','}',';',',', '[', ']'};
-char operator_sym[10] = {'=','<','>','!','+','-','/','*','&','|'};
-char identifier_sym[37] = {'a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '_'};
-char *relops[6]={"==", "!=", "<=", ">=", ">", "<"};
-char *mulops[3]={"*", "/", "%"};
-char *addops[2]={"+", "-"};
-const int alphanum=0, specialchar=1, operator=2;
-int id_count=0;
-char *datatype[6] = {"int", "char"};
+char spec_char[8] = {
+    '(',
+    ')',
+    '{',
+    '}',
+    ';',
+    ',',
+    '[',
+    ']'
+};
+char operator_sym[10] = {
+    '=',
+    '<',
+    '>',
+    '!',
+    '+',
+    '-',
+    '/',
+    '*',
+    '&',
+    '|'
+};
+char identifier_sym[37] = {
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '_'
+};
+char * relops[6] = {
+    "==",
+    "!=",
+    "<=",
+    ">=",
+    ">",
+    "<"
+};
+char * mulops[3] = {
+    "*",
+    "/",
+    "%"
+};
+char * addops[2] = {
+    "+",
+    "-"
+};
+const int alphanum = 0,
+    specialchar = 1,
+    operator = 2;
+int id_count = 0;
+char * datatype[6] = {
+    "int",
+    "char"
+};
 int cur_flag = -1;
-int line=1;
+int line = 1;
 
-typedef struct symboltable
-{
+typedef struct symboltable {
     int index;
-    char *name;
-    char *type;
+    char * name;
+    char * type;
     int size;
     char scope;
     int arg_no;
     int args[10];
-    char *returntype;
-    struct symboltable *next;
-}symtable;
+    char * returntype;
+    struct symboltable * next;
+}
+symtable;
 
-symtable **array;
+symtable * * array;
 
-void insert (symtable entry)
-{
-    int len=strlen(entry.name);
-    if (array[len%8]==NULL)
-    {
-        array[len%8]=malloc(sizeof(symtable));
-        *(array[len%8]) = entry;
-        array[len%8]->next=NULL;
+void insert(symtable entry) {
+    int len = strlen(entry.name);
+    if (array[len % 8] == NULL) {
+        array[len % 8] = malloc(sizeof(symtable)); * (array[len % 8]) = entry;
+        array[len % 8] - > next = NULL;
+    } else {
+        symtable * temp = array[len % 8];
+        while (temp - > next != NULL)
+            temp = temp - > next;
+        temp - > next = malloc(sizeof(symtable)); * (temp - > next) = entry;
+        temp - > next - > next = NULL;
     }
-    else
-    {
-        symtable *temp=array[len%8];
-        while (temp->next != NULL)
-            temp=temp->next;
-        temp->next = malloc(sizeof(symtable));
-        *(temp->next) = entry;
-        temp->next->next=NULL;
-    }
-
 
 }
 
-void print_sym_table()
-{
+void print_sym_table() {
     int i;
-    symtable *temp;
+    symtable * temp;
     printf("\nIndex\tName\tType\tSize\tScope\tNo of Arguments\tArguments\tReturn Type\n");
-    for (i=0;i<8;i++)
-    {
+    for (i = 0; i < 8; i++) {
         temp = array[i];
-        while (temp != NULL)
-        {
-            printf("%d\t%s\t%s",temp->index, temp->name, temp->type);
-            if (strcmp(temp->type,"function")==0)
-            {
-                printf("\t\t%c\t%d\t",temp->scope,temp->arg_no);
-                if (temp->arg_no==0)printf("NULL");
-                else
-                {
+        while (temp != NULL) {
+            printf("%d\t%s\t%s", temp - > index, temp - > name, temp - > type);
+            if (strcmp(temp - > type, "function") == 0) {
+                printf("\t\t%c\t%d\t", temp - > scope, temp - > arg_no);
+                if (temp - > arg_no == 0) printf("NULL");
+                else {
                     int p;
-                    for (p=0;p<temp->arg_no;p++)
-                        printf("<id, %d> ", temp->args[p]);
+                    for (p = 0; p < temp - > arg_no; p++)
+                        printf("<id, %d> ", temp - > args[p]);
                 }
-                printf("\t%s\n", temp->returntype);
-            }
+                printf("\t%s\n", temp - > returntype);
+            } else printf("\t%d\t%c\n", temp - > size, temp - > scope);
 
-            else printf("\t%d\t%c\n", temp->size, temp->scope);
-
-            temp = temp->next;
+            temp = temp - > next;
         }
     }
 }
 
-int search_sym_table(char *buffer)
-{
-    int i;
-    symtable *temp;
-    temp = array[strlen(buffer)%8];
-    while (temp!=NULL)
-    {
-        if (strcmp(temp->name, buffer)==0)
-            return temp->index;
-        temp=temp->next;
+int search_sym_table(char * buffer) {
+        int i;
+        symtable * temp;
+        temp = array[strlen(buffer) % 8];
+        while (temp != NULL) {
+            if (strcmp(temp - > name, buffer) == 0)
+                return temp - > index;
+            temp = temp - > next;
+        }
+        return -1;
     }
-    return -1;
-}
-//Character Search in an array of characters
-int is_found(char c, char array[], int len)
-{
+    //Character Search in an array of characters
+int is_found(char c, char array[], int len) {
+        int i;
+        for (i = 0; i < len; i++)
+            if (c == array[i]) return 1;
+        return 0;
+    }
+    //Search String in a String array 
+int is_found_string(char * string, char * arr[], int n) {
     int i;
-    for (i=0;i<len;i++)
-        if (c == array[i])return 1;
-    return 0;
-}
-//Search String in a String array 
-int is_found_string(char *string, char *arr[], int n)
-{
-    int i;
-    for (i=0;i<n;i++)
-    {
-        if (strcmp(string,arr[i])==0)
-        {
+    for (i = 0; i < n; i++) {
+        if (strcmp(string, arr[i]) == 0) {
             return i;
         }
     }
     return -1;
 }
 
-int get_char_class(char c)
-{
-    if (is_found(c, spec_char, 8))return specialchar;
-    else if (is_found(c, operator_sym, 10))return operator;
-    else if (is_found(c, identifier_sym, 37))return alphanum;
+int get_char_class(char c) {
+    if (is_found(c, spec_char, 8)) return specialchar;
+    else if (is_found(c, operator_sym, 10)) return operator;
+    else if (is_found(c, identifier_sym, 37)) return alphanum;
     else return -1;
 }
 
-void handle_exceptions(char c, FILE *src)
-{
-    if (c == '\"')
-    {
+void handle_exceptions(char c, FILE * src) {
+    if (c == '\"') {
         c = fgetc(src);
-        while (c!='\"')
-        {
-            if (c=='\n')line++;
+        while (c != '\"') {
+            if (c == '\n') line++;
             c = fgetc(src);
         }
-    }
-    else if (c == '\'')
-    {
+    } else if (c == '\'') {
         c = fgetc(src);
-        while (c!='\'')
-        {
-            if (c=='\n')line++;
+        while (c != '\'') {
+            if (c == '\n') line++;
             c = fgetc(src);
         }
-    }
-    else if (c == '#')
-    {
-        while(c!='\n')
+    } else if (c == '#') {
+        while (c != '\n')
             c = fgetc(src);
         line++;
-    }
-    else if (c=='\n')
-    {
+    } else if (c == '\n') {
         line++;
     }
 }
 
-int is_identifier(char *buffer)
-{
+int is_identifier(char * buffer) {
     int len = strlen(buffer);
     int i;
-    for (i=0;i<len;i++)
-    {
-        if (!((buffer[i]>=65 && buffer[i]<91) || (buffer[i]>=97 && buffer[i]<123) || (buffer[i]=='_')))
+    for (i = 0; i < len; i++) {
+        if (!((buffer[i] >= 65 && buffer[i] < 91) || (buffer[i] >= 97 && buffer[i] < 123) || (buffer[i] == '_')))
             return 0;
     }
     return 1;
 }
 
-int find_token(char *buffer, FILE *src)
-{
+int find_token(char * buffer, FILE * src) {
     char c = fgetc(src);
     int this_class = get_char_class(c);
-    int k=0;
-    if (c == EOF)
-    {
+    int k = 0;
+    if (c == EOF) {
         buffer[0] = EOF;
         return -1;
     }
-    if (this_class<0)
-    {
+    if (this_class < 0) {
         handle_exceptions(c, src);
-        return find_token(buffer,src);
+        return find_token(buffer, src);
     }
-    if (this_class == 1)
-    {
+    if (this_class == 1) {
         buffer[0] = c;
         buffer[1] = '\0';
-        if (strcmp(buffer, ";")==0 || strcmp(buffer, "{")==0)cur_flag=-1;
+        if (strcmp(buffer, ";") == 0 || strcmp(buffer, "{") == 0) cur_flag = -1;
         return this_class;
     }
-    while (get_char_class(c) == this_class && c != EOF)
-    {
+    while (get_char_class(c) == this_class && c != EOF) {
         buffer[k++] = c;
         c = fgetc(src);
     }
-    buffer[k]='\0';
-    if (is_identifier(buffer))
-    {
-        if (is_found_string(buffer, keywords, 32)>=0)
-        {
+    buffer[k] = '\0';
+    if (is_identifier(buffer)) {
+        if (is_found_string(buffer, keywords, 32) >= 0) {
             int n;
-            if ((n=is_found_string(buffer, datatype, 2))>=0)
-            {
+            if ((n = is_found_string(buffer, datatype, 2)) >= 0) {
                 cur_flag = n;
             }
-        }
-        else if (cur_flag>=0)
-        {
-            if (cur_flag != 5)
-            {
+        } else if (cur_flag >= 0) {
+            if (cur_flag != 5) {
 
                 symtable entry;
                 entry.index = ++id_count;
@@ -238,7 +258,7 @@ int find_token(char *buffer, FILE *src)
                 strcpy(entry.name, buffer);
                 entry.type = malloc(10);
                 strcpy(entry.type, datatype[cur_flag]);
-                entry.size = cur_flag == 0?4:1;
+                entry.size = cur_flag == 0 ? 4 : 1;
                 entry.scope = 'l';
                 insert(entry);
             }
@@ -249,15 +269,11 @@ int find_token(char *buffer, FILE *src)
     return this_class;
 }
 
-
-
-int is_number(char *buffer)
-{
-    int len=strlen(buffer);
+int is_number(char * buffer) {
+    int len = strlen(buffer);
     int i;
-    for (i=0;i<len;i++)
-    {
-        if (!((buffer[i]>='0')&&(buffer[i]<='9')))
+    for (i = 0; i < len; i++) {
+        if (!((buffer[i] >= '0') && (buffer[i] <= '9')))
             return 0;
 
     }
--- main.c
+++ main.c
@@ -1,16 +1,102 @@
-#include "q2.c"
+#
+include "q2.c"
 
-char *cur_token;
-FILE *inputfile;
-char *first_declarations[2]={"int", "char"};
-int error_flag=0;
+char * cur_token;
+FILE * inputfile;
+char * first_declarations[2] = {
+    "int",
+    "char"
+};
+int error_flag = 0;
 
 void picture();
 
-void error(char *expected)
-{
+void error(char * expected) {
     printf("Error in parsing at line %d. %s expected\n", line, expected);
-    error_flag=1;
+    error_flag = 1;
+}
+
+/*
+    Check First(command) set compliance
+*/
+int check_first_command() {
+
+    if (
+        // <primitive> [<attribute>]
+        is_found_string(cur_token, primitive_set, 10) || is_text(cur_token)
+        // LABEL : [;] <command>
+        // LABEL : [;] <command> [<position>]
+        is_label(cur_token) ||
+        // { <command> ... }
+        strcmp(cur_token, "{") == 0 ||
+        // VARIABLE [:] = <any-expr>
+        is_variable(cur_token) ||
+        // up | down | left | right
+        is_found_string(cur_token, direction_set, 4) ||
+        // COMMAND-LINE
+        is_command_line(cur_token) ||
+        // command <print-arg> ...
+        strcmp(cur_token, "command") == 0 ||
+        // print <print-arg> ...
+        strcmp(cur_token, "print") == 0 ||
+        // sh BALANCED-TEXT
+        strcmp(cur_token, "sh") == 0 ||
+        /*
+        copy FILENAME
+        copy [FILENAME] thru MACRONAME [until TEXT]
+        copy [FILENAME] thru BALANCED-BODY [until TEXT]
+        */
+        strcmp(cur_token, "copy") == 0 ||
+        // for VARIABLE = <expr> to <expr> [by [*] <expr>] do BALANCED-BODY
+        strcmp(cur_token, "for") == 0 ||
+        // if <any-expr> then BALANCED-BODY [else BALANCED-BODY]
+        strcmp(cur_token, "if") == 0 ||
+        // reset [VARIABLE [[,] VARIABLE ...]]
+        strcmp(cur_token, "reset") == 0
+    ) {
+        return 1;
+    }
+
+    return 0;
+}
+
+/*
+    Check First(primitive) compliance
+
+    <primitive> ::=
+    box                   # closed object — rectangle
+    circle                # closed object — circle
+    ellipse               # closed object — ellipse
+    arc                   # open object — quarter-circle
+    line                  # open object — line
+    arrow                 # open object — line with arrowhead
+    spline                # open object — spline curve
+    move
+    TEXT TEXT ...         # text within invisible box
+    plot <expr> TEXT      # formatted text
+    ’[’ <command> ... ’]’
+**/
+
+void check_first_primitive() {
+
+    char * primitive_set[10] = {
+        "box",
+        "circle",
+        "ellipse",
+        "arc",
+        "line",
+        "arrow",
+        "spline",
+        "move",
+        "plot",
+        "["
+    };
+
+    if (is_found_string(cur_token, primitive_set, 10) || is_text(cur_token)) {
+        return 1;
+    }
+
+    return 0;
 }
 
 /**
@@ -21,41 +107,37 @@ void error(char *expected)
   */
 
 void picture() {
+
+    find_token(cur_token, inputfile);
+
     if (strcmp(cur_token, ".PS") == 0) {
+        printf("Matched to %s\n", cur_token);
+
         find_token(cur_token, inputfile);
-        printf("Pointing to %s\n",cur_token);
-        
-        if(is_number(cur_token)) {
+
+        if (is_number(cur_token)) {
+            printf("Matched to %s\n", cur_token);
             find_token(cur_token, inputfile);
-            printf("Pointing to %s\n",cur_token);
 
-            if(is_number(cur_token)) {
+            if (is_number(cur_token)) {
+                printf("Matched to %s\n", cur_token);
                 find_token(cur_token, inputfile);
-                printf("Pointing to %s\n",cur_token);
-
-                if(is_found_string(curr_token, primitive, 10) || is_text(cur_token)) {
-                    find_token(cur_token, inputfile);
-                    printf("Pointing to %s\n",cur_token);
-
-                    statement();
-                }
-                if(strcmp(cur_token, ".PE") == 0) {
-                    find_token(cur_token, inputfile);
-                    printf("Pointing to %s\n",cur_token);
-                }
-                else {
-                    error(".PE in Picture");
-                }
             }
-            else {
-                error("NUMBER in Picture");
-            }           
         }
-        else {
-            error("NUMBER in Picture");
+
+        // first(statement) = first(command)
+        while (check_first_command() == 1) {
+            fseek(inputfile, -1 * strlen(cur_token), SEEK_CUR);
+            statement();
+            find_token(cur_token, inputfile);
         }
-    }
-    else {
+
+        if (strcmp(cur_token, ".PE") == 0) {
+            printf("Matched to %s\n", cur_token);
+        } else {
+            error(".PE in Picture");
+        }
+    } else {
         error(".PS in Picture");
     }
 
@@ -68,67 +150,308 @@ void picture() {
 **/
 
 void statement() {
-    if(
+    find_token(cur_token, inputfile);
+
+    if (check_first_command() == 1) {
+        fseek(inputfile, -1 * strlen(cur_token), SEEK_CUR);
+
+        command();
+
+        find_token(cur_token, inputfile);
+        if (strcmp(cur_token, ";") == 0) {
+            printf("Matched to %s\n", cur_token);
+        } else {
+            fseek(inputfile, -1 * strlen(cur_token), SEEK_CUR);
+        }
+    } else {
+        error("command in statement");
+    }
+}
+
+/**
+<command> ::=
+  <primitive> [<attribute>]
+  LABEL : [;] <command>
+  LABEL : [;] <command> [<position>]
+  { <command> ... }
+  VARIABLE [:] = <any-expr>
+  up | down | left | right
+  COMMAND-LINE
+  command <print-arg> ...
+  print <print-arg> ...
+  sh BALANCED-TEXT
+  copy FILENAME
+  copy [FILENAME] thru MACRONAME [until TEXT]
+  copy [FILENAME] thru BALANCED-BODY [until TEXT]
+  for VARIABLE = <expr> to <expr> [by [*] <expr>] do BALANCED-BODY
+  if <any-expr> then BALANCED-BODY [else BALANCED-BODY]
+  reset [VARIABLE [[,] VARIABLE ...]]
+  **/
+
+void command() {
+    find_token(cur_token, inputfile);
+
     // <primitive> [<attribute>]
-    is_found_string(curr_token, primitive, 10) ||
-    // LABEL : [;] <command>
-    // LABEL : [;] <command> [<position>]
-    is_label(curr_token) ||
+    if (is_found_string(cur_token, primitive, 10) || is_text(cur_token)) {
+        printf("Matched to %s\n", cur_token);
+
+        find_token(cur_token, inputfile);
+        if (check_first_attribute() == 1) {
+            attribute();
+        } else {
+            fseek(inputfile, -1 * strlen(cur_token), SEEK_CUR);
+        }
+    }
+
+    /**
+      LABEL : [;] <command>
+      LABEL : [;] <command> [<position>]
+    **/
+    else if (strcmp(cur_token, "LABEL") == 0) {
+        printf("Matched to %s\n", cur_token);
+
+        find_token(cur_token, inputfile);
+        if (strcmp(cur_token, ":") == 0) {
+            printf("Matched to %s\n", cur_token);
+
+            find_token(cur_token, inputfile);
+            if (strcmp(cur_token, ";") == 0) {
+                printf("Matched to %s\n", cur_token);
+            } else {
+                fseek(inputfile, -1 * strlen(cur_token), SEEK_CUR);
+            }
+
+            command();
+
+            find_token(cur_token, inputfile);
+            if (check_first_position() == 1) {
+                position();
+            } else {
+                fseek(inputfile, -1 * strlen(cur_token), SEEK_CUR);
+            }
+        } else {
+            error("; in command");
+        }
+    }
+
     // { <command> ... }
-    strcmp(cur_token, "{") == 0 ||
+    else if (strcmp(cur_token, "{") == 0) {
+        printf("Matched to %s\n", cur_token);
+
+        find_token(cur_token, inputfile);
+
+        while (check_first_command() == 1) {
+            fseek(inputfile, -1 * strlen(cur_token), SEEK_CUR);
+            command();
+            find_token(cur_token, inputfile);
+        }
+        
+        if (strcmp(cur_token, "}") == 0) {
+            printf("Matched to %s\n", cur_token);
+        } else {
+            error("} in command");
+        }
+    }
+
     // VARIABLE [:] = <any-expr>
-    is_variable(curr_token) ||
+    else if (check_first_variable() == 1) {
+
+        find_token(cur_token, inputfile);
+
+        if (strcmp(cur_token, ":") == 0) {
+            printf("Matched to %s\n", cur_token);
+            find_token(cur_token, inputfile);
+        }
+
+        if (strcmp(cur_token, "=") == 0) {
+            printf("Matched to %s\n", cur_token);
+
+            any_expr();
+        } else {
+            error("= in command");
+        }
+
+    } 
     // up | down | left | right
-    is_direction(curr_token) ||
-    // COMMAND-LINE
-    is_command_line(curr_token) ||
+    else if(is_found_string(cur_token, direction_set, 4)) {
+        printf("Matched to %s\n", cur_token);
+    }
+    //COMMAND-LINE
+    else if(is_command_line() == 1) {
+        printf("Matched to %s\n", cur_token);
+    }
     // command <print-arg> ...
-    strcmp(curr_token, "command") == 0 ||
     // print <print-arg> ...
-    strcmp(curr_token, "print") == 0 ||
-    // sh BALANCED-TEXT
-    strcmp(curr_token, "sh") == 0 ||
+    else if (strcmp(cur_token, "command") == 0 || strcmp(cur_token, "print") == 0) {
+        find_token(cur_token, inputfile);
+
+        while(check_first_print_arg() == 1) {
+            fseek(inputfile, -1 * strlen(cur_token), SEEK_CUR);
+            print_arg();
+            find_token(cur_token, inputfile);
+        }
+        fseek(inputfile, -1 * strlen(cur_token), SEEK_CUR);
+    }
+    else if (strcmp(cur_token, "sh") == 0) {
+        find_token(cur_token, inputfile);
+
+        if (is_balanced_body(cur_token) == 0) {
+            printf("Matched to %s\n", cur_token);
+        } else {
+            error("balanced text in command");
+        }
+    }
     /*
+    Skipped:
     copy FILENAME
     copy [FILENAME] thru MACRONAME [until TEXT]
     copy [FILENAME] thru BALANCED-BODY [until TEXT]
     */
-    strcmp(curr_token, "copy") == 0 ||
+
     // for VARIABLE = <expr> to <expr> [by [*] <expr>] do BALANCED-BODY
-    strcmp(curr_token, "for") == 0 ||
-    // if <any-expr> then BALANCED-BODY [else BALANCED-BODY]
-    strcmp(curr_token, "if") == 0 ||
-    // reset [VARIABLE [[,] VARIABLE ...]]
-    strcmp(curr_token, "reset") == 0
-    ) {
-        command();
-        if(strcmp(curr_token, ";") == 0) {
+    else if(strcmp(cur_token, "for") == 0) {
+        find_token(cur_token, inputfile);
+
+        if(is_variable(cur_token) == 0) {
+            printf("Matched to %s\n", cur_token);
+
             find_token(cur_token, inputfile);
-            printf("Pointing to %s\n",cur_token);
-        }       
+            if(strcmp(cur_token, "=") == 0) {
+                printf("Matched to %s\n", cur_token);
+                find_token(cur_token, inputfile);
 
+                if(is_expr(cur_token) == 1) {
+                    printf("Matched to %s\n", cur_token);
+                    find_token(cur_token, inputfile);
+                    
+                    if(strcmp(cur_token, "to") == 0) {
+                        printf("Matched to %s\n", cur_token);
+                        find_token(cur_token, inputfile);
+                        
+                        if(is_expr(cur_token) == 1) {
+                            printf("Matched to %s\n", cur_token);
+                            
+                            find_token(cur_token, inputfile);
+
+                            if(strcmp(cur_token, "by") == 0) {
+                                printf("Matched to %s\n", cur_token);
+                                find_token(cur_token, inputfile);
+
+                                if(strcmp(cur_token, "*") == 0) {
+                                    printf("Matched to %s\n", cur_token);
+                                    find_token(cur_token, inputfile);
+                                }
+
+                                if(is_expr(cur_token) == 1) {
+                                    printf("Matched to %s\n", cur_token);
+                                    find_token(cur_token, inputfile);
+                                }
+                                else {
+                                    error("Expr in command");
+                                }
+                            }
+
+                            if(strcmp(cur_token, "do") == 0) {
+                                printf("Matched to %s\n", cur_token);
+                                find_token(cur_token, inputfile);
+
+                                if(is_balanced_body(cur_token) == 1) {
+                                    printf("Matched to %s\n", cur_token);
+                                }
+                                else {
+                                    error("Balanced_text in command");
+                                }
+                            }
+                            else {
+                                error("do in command");
+                            }
+
+                        } else {
+                            error("expr in command");
+                        }
+                        
+                    } else {
+                        error("expr in command");
+                    }
+                } else {
+                    error("expr in command");
+                }
+            } else {
+                error("= in command");
+            }
+        } else {
+            error("VARIABLE in command");
+        }
     }
-}
 
+    // if <any-expr> then BALANCED-BODY [else BALANCED-BODY]
+    else if(strcmp(cur_token, "if") == 0) {
+        find_token(cur_token, inputfile);
+        if(is_any_expr(cur_token) == 1) {
+            printf("Matched to %s\n", cur_token);
+            find_token(cur_token, inputfile);
+            
+            if(strcmp(cur_token, "then") == 0) {
+                printf("Matched to %s\n", cur_token);
+                find_token(cur_token, inputfile);
+            
+                if(is_balanced_body(cur_token) == 1) {
+                    printf("Matched to %s\n", cur_token);
+                    find_token(cur_token, inputfile);
+
+                    if(strcmp(cur_token, "else") == 0) {
+                        printf("Matched to %s\n", cur_token);
+                        find_token(cur_token, inputfile);
+
+                        if(is_balanced_body(cur_token) == 1) {
+                            printf("Matched to %s\n", cur_token);
+                        } else {
+                            error("balanced_body in command");
+                        }
+                    } else {
+                        fseek(inputfile, -1 * strlen(cur_token), SEEK_CUR);
+                    }                   
+                } else {
+                    error("any_expr in command");
+                }
+                
+            } else {
+                error("any_expr in command");
+            }
+
+        } else {
+            error("any_expr in command");
+        }
+    }
+    /*
+        Skipped:
+        reset [VARIABLE [[,] VARIABLE ...]]
+    */
+    else {
+        error("Expected first(statement)");
+    }
+
+
+}
 
 int main() {
     int count = 0;
     char filename[20];
-    cur_token=malloc(20);
-    array=malloc(8*sizeof(symtable*));
+    cur_token = malloc(20);
+    array = malloc(8 * sizeof(symtable * ));
     printf("Enter filename: ");
     scanf("%s", filename);
     inputfile = fopen(filename, "r");
     char c;
     int i;
-    find_token(cur_token, inputfile);
-    printf("Pointing to %s\n",cur_token);
-    program();
-    if (cur_token[0]==EOF)
-    {
-        if (!error_flag)
+
+    picture();
+    if (cur_token[0] == EOF) {
+        if (!error_flag) {
             printf("Success\n");
+        }
+    } else {
+        error("End of file");
     }
-    else error("End of file");
     print_sym_table();
 }
